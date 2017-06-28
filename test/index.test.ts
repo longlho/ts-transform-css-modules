70b6f080808e5b2ae77497a75f29bc896c8b5a1e
Long Ho <longho@yahoo-inc.com>
2017-06-27 23:09:52 -0400

    increate mocha timeout

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { expect } from 'chai'
 
 describe('ts-transform-css-modules', function () {
+    this.timeout(5000)
     it('should be able to compile CSS', function () {
         compile(resolve(__dirname, 'fixture/*.ts'))
         expect(require('./fixture/index.js').default()).to.deep.equal({
